0bcf5f61a4c705770b1a6467553f380b2252b11e
Hortense Cauchois <hortensecauchois@gmail.com>
2024-05-27 16:24:03 +0200

    fixed diverses bugs

--- app/src/Difficulty.py
+++ app/src/Difficulty.py
@@ -1,6 +1,18 @@
 from enum import Enum
 
 
+def get_difficulty_by_number(number):
+    match number:
+        case 2:
+            return Difficulty.EASY
+        case 4:
+            return Difficulty.MEDIUM
+        case 6:
+            return Difficulty.HARD
+        case _:
+            return Difficulty.EASY
+
+
 class Difficulty(Enum):
     EASY = 2
     MEDIUM = 4
